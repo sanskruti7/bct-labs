dcba34ef93bb47efe6e9ac2a073b25326eb65bc1
sanskruti7 <sanskrutibadhe7@gmail.com>
2022-10-11 09:40:50 +0530

    first commit

---
+++ lottery.sol
@@ -0,0 +1,41 @@
+//Sanskruti Badhe
+//2020301072
+//SPDX-License-Identifier : MIT
+pragma solidity ^0.8.0;
+contract Lottery{
+    address public manager;
+    address payable[] public participants;
+
+    constructor(){
+        manager = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(manager == msg.sender, "Only Manager can access this function");
+        _;
+    }
+
+    receive() external payable{
+        require(msg.value == 1 ether);
+        participants.push(payable(msg.sender));
+    }
+
+    function getBalance() public onlyOwner view returns(uint){
+        return address(this).balance;
+    }
+
+    function random() public view returns(uint){
+        return uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty, participants.length)));
+    }
+
+    function selectWinner() public onlyOwner returns(address){
+        require(participants.length >= 3);
+        address payable winner;
+        uint r = random();
+        uint index = r % participants.length;
+        winner = participants[index];
+        winner.transfer(getBalance());
+        participants = new address payable[](0);
+        return winner;
+    }
+}
